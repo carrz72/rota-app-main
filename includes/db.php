c7e57b1bb7ddec38c79c8d1c1b27386b20b02d7f
carrz72 <carrz723@gmail.com>
2025-03-25 21:14:23 +0000

    db change

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $dbname = 'rota_app';
-$username = 'root';
-$password = '';
+$username = 'admin';
+$password = 'd4f57d3693e34f12f5e1aaffcf0f3a7a0bb33ea9ff60845f';
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
