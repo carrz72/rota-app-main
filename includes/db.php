6763467e956e17bdc8e1288e95989f2c6a81bfd1
carrz72 <161781140+carrz72@users.noreply.github.com>
2025-04-04 10:33:35 +0100

    test

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,17 @@
+// filepath: /var/www/rota-app/includes/db.php
 <?php
-$host = 'localhost';
-$dbname = 'rota_app';
-$username = 'admin';
-$password = 'd4f57d3693e34f12f5e1aaffcf0f3a7a0bb33ea9ff60845f';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+// Load environment variables
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
