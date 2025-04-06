d57347f584dcf8eabcc184ea46a7720b317e3f6f
carrz72 <161781140+carrz72@users.noreply.github.com>
2025-04-06 03:09:29 +0100

    fixes

--- includes/header.php
+++ includes/header.php
@@ -19,6 +19,7 @@ if ($user_id) {
 <meta name="apple-mobile-web-app-title" content="Open Rota">
 <link rel="icon" type="image/png" href="/rota-app-main/images/logo.png">
 <link rel="manifest" href="/rota-app-main/manifest.json">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="apple-touch-icon" href="/rota-app-main/images/logo.png">
     <div class="logo">Open Rota.</div>
 <div class="nav-group">
